6d95f6de513c3ee4f15f0739c58a956b319f3ed1
13 <13@users.noreply.github.com>
2026-01-19 21:51:58 +0100

    added .muh

--- mqtt-status.sh
+++ mqtt-status.sh
@@ -2,7 +2,8 @@
 # Publish minimal online/offline status to MQTT
 
 BROKER="${MQTT_BROKER:-192.168.22.5}"
-HOSTNAME=$(hostname | tr 'A-Z' 'a-z')
+#HOSTNAME=$(hostname | tr 'A-Z' 'a-z')
+HOSTNAME="$(hostname | tr 'A-Z' 'a-z').muh"
 IP=$(hostname -I | awk '{print $1}')
 MAC=$(cat /sys/class/net/*/address | grep -Ev '^00:00:00' | head -n1 | tr 'A-Z' 'a-z')
 TOPIC="muh/pc/$HOSTNAME"
